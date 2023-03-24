edade1de00bb44330f6298fc6484fdc747758fb5
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-03-23 20:53:13 -0400

    Fixed Colorful Glass Pane

--- src/main/java/com/kevinthegreat/colorfulworld/ColorfulWorldClient.java
+++ src/main/java/com/kevinthegreat/colorfulworld/ColorfulWorldClient.java
@@ -10,7 +10,7 @@ import net.minecraft.client.render.RenderLayer;
 public class ColorfulWorldClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ColorfulWorld.COLORFUL_GLASS, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(), ColorfulWorld.COLORFUL_GLASS, ColorfulWorld.COLORFUL_GLASS_PANE);
         ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> view != null && pos != null ? (int) view.getBlockEntity(pos, ColorfulWorld.COLORFUL_BLOCK_ENTITY).or(() -> view.getBlockEntity(pos.down(), ColorfulWorld.COLORFUL_BLOCK_ENTITY)).map(ColorfulBlockEntity::getRenderAttachmentData).orElse(0) : 0, ColorfulWorld.COLORFUL_CONCRETE, ColorfulWorld.COLORFUL_CONCRETE_POWDER, ColorfulWorld.COLORFUL_GLASS, ColorfulWorld.COLORFUL_GLASS_PANE);
         ColorProviderRegistry.ITEM.register((stack, tintIndex) -> Util.getColorFromStack(stack), ColorfulWorld.COLORFUL_CONCRETE_ITEM, ColorfulWorld.COLORFUL_CONCRETE_POWDER_ITEM, ColorfulWorld.COLORFUL_GLASS_ITEM, ColorfulWorld.COLORFUL_GLASS_PANE_ITEM);
     }
