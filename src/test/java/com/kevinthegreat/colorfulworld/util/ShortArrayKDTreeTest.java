6cc351092d33a8efa4181c4d737f48eba52b895b
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-04-19 22:51:27 -0400

    Update build.gradle datagen and and test

--- src/test/java/com/kevinthegreat/colorfulworld/util/ShortArrayKDTreeTest.java
+++ src/test/java/com/kevinthegreat/colorfulworld/util/ShortArrayKDTreeTest.java
@@ -1,22 +1,26 @@
 package com.kevinthegreat.colorfulworld.util;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 public class ShortArrayKDTreeTest {
-    public static void main(String[] args) {
+    @Test
+    void testShortArrayKDTree() {
         short[][] values = new short[][]{{7, 2}, {5, 4}, {9, 6}, {4, 7}, {8, 1}, {2, 3}};
         ShortArrayKDTree tree = new ShortArrayKDTree(2, values);
-        assert Arrays.equals(tree.getNearest(new short[]{2, 2}), new short[]{2, 3});
-        assert Arrays.equals(tree.getNearest(new short[]{5, 1}), new short[]{7, 2});
-        assert Arrays.equals(tree.getNearest(new short[]{5, 3}), new short[]{5, 4});
-        assert Arrays.equals(tree.getNearest(new short[]{8, 3}), new short[]{7, 2});
-        assert Arrays.equals(tree.getNearest(new short[]{8, 3}), new short[]{7, 2});
-        assert Arrays.equals(tree.getNearest(new short[]{7, 6}), new short[]{9, 6});
+        Assertions.assertArrayEquals(new short[]{2, 3}, tree.getNearest(new short[]{2, 2}));
+        Assertions.assertArrayEquals(new short[]{7, 2}, tree.getNearest(new short[]{5, 1}));
+        Assertions.assertArrayEquals(new short[]{5, 4}, tree.getNearest(new short[]{5, 3}));
+        Assertions.assertArrayEquals(new short[]{7, 2}, tree.getNearest(new short[]{8, 3}));
+        Assertions.assertArrayEquals(new short[]{7, 2}, tree.getNearest(new short[]{8, 3}));
+        Assertions.assertArrayEquals(new short[]{9, 6}, tree.getNearest(new short[]{7, 6}));
         short[] nearest = tree.getNearest(new short[]{7, 4});
-        assert Arrays.equals(nearest, new short[]{7, 2}) || Arrays.equals(nearest, new short[]{5, 4});
+        Assertions.assertTrue(Arrays.equals(nearest, new short[]{7, 2}) || Arrays.equals(nearest, new short[]{5, 4}));
         nearest = tree.getNearest(new short[]{7, 5});
-        assert Arrays.equals(nearest, new short[]{5, 4}) || Arrays.equals(nearest, new short[]{9, 6});
+        Assertions.assertTrue(Arrays.equals(nearest, new short[]{5, 4}) || Arrays.equals(nearest, new short[]{9, 6}));
         nearest = tree.getNearest(new short[]{6, 6});
-        assert Arrays.equals(nearest, new short[]{5, 4}) || Arrays.equals(nearest, new short[]{4, 7});
+        Assertions.assertTrue(Arrays.equals(nearest, new short[]{5, 4}) || Arrays.equals(nearest, new short[]{4, 7}));
     }
 }
